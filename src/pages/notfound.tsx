a842c3521ebc87e562a9ffde24e02fbd9d6e963d
iradukun <mustaphairadukunda@gmail.com>
2023-03-14 13:00:17 +0200

    [add] not found page

---
+++ src/pages/notfound.tsx
@@ -0,0 +1,10 @@
+import RouteProtector from '@/utils/RouteProtection'
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <RouteProtector>NotFound</RouteProtector>
+  )
+}
+
+export default NotFound
