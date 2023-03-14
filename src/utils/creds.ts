b72e0b6d16ac4a53797adb5e52b9ccbe40936d8f
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-03-14 20:05:10 +0200

    [add] instagram credentials

--- src/utils/creds.ts
+++ src/utils/creds.ts
@@ -1,10 +1,15 @@
 interface externalAuth {
     client_id:string;
     client_secret:string;
-    callback_url:String;
+    callback_url:string;
 }
 export const Twitch : externalAuth = {
     client_id: '4nu0djcjtxkf8pk6di63mlqlgpjh4t',
     client_secret: 'saej3t6vh1dzgyi0sn19mls5u2jcrr',
-    callback_url: 'http://localhost/3000'
+    callback_url: 'http://localhost:3000'
+}
+export const Instagram : externalAuth = {
+    client_id: '1393598464512376',
+    client_secret: '571af429640840fe27f4094bfacb7b62',
+    callback_url: 'http://localhost:3000'
 }
