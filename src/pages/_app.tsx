e90cdbb9bd83205dfb5bcbb4948e96171ef677e7
iradukun <mustaphairadukunda@gmail.com>
2023-03-14 13:01:08 +0200

    [add] google provider referencing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import type { AppProps } from 'next/app'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <GoogleOAuthProvider clientId='1071226693678-ckfpvtck6kirqh6m9um4a59t4984rs2d.apps.googleusercontent.com'>
+      <ToastContainer style={{ fontSize: '0.8em' }} />
+      <Component {...pageProps} />
+    </GoogleOAuthProvider>
+  )
 }
