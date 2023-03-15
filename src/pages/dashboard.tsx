6f4502be10d0cde4f82318e86c87afd0b21f6bad
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-03-15 11:40:38 +0200

    [add] fixing cors

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -12,9 +12,10 @@ const Dashboard = () => {
 
     const response = await fetch(url, {
       method: 'POST',
+      mode: 'no-cors',
       headers: {
-        'Access-Control-Allow-Origin' : 'https://api.instagram.com/oauth/access_token',
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'Access-Control-Allow-Origin' : '*',
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
       body: `client_id=${clientID}&client_secret=${clientSecret}&grant_type=${grantType}&redirect_uri=${redirectURI}&code=${authorizationCode}`
     });
