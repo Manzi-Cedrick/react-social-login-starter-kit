be6577c3655760d381028114299569c0ca18fb9e
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-03-15 13:05:39 +0200

    [add] mode change

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,6 +3,31 @@ import { useRouter } from 'next/router'
 import axios from 'axios';
 
 const Dashboard = () => {
+  const fetchInstagramProfile = async (authorizationCode: any) => {
+    const clientID = '2040498642821593';
+    const clientSecret = '07fd638959f54656f00f2f71d9dee9ce';
+    const redirectURI = 'https://react-social-login-starter-kit.vercel.app/dashboard';
+    const grantType = 'authorization_code';
+    const url = 'https://cors-anywhere.herokuapp.com/https://api.instagram.com/oauth/access_token';
+
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Access-Control-Allow-Origin' : '*',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `client_id=${clientID}&client_secret=${clientSecret}&grant_type=${grantType}&redirect_uri=${redirectURI}&code=${authorizationCode}`
+    });
+
+    const { access_token } = await response.json();
+
+    console.log("the response: ",response)
+    const profileResponse = await fetch(`https://graph.instagram.com/me?fields=id,username&access_token=${access_token}`);
+    const profile = await profileResponse.json();
+    console.log("the profile: ",profile)
+    return { access_token, profile };
+  };
   // const fetchInstagramProfile = async (authorizationCode: any) => {
   //   const clientID = '2040498642821593';
   //   const clientSecret = '07fd638959f54656f00f2f71d9dee9ce';
@@ -10,58 +35,33 @@ const Dashboard = () => {
   //   const grantType = 'authorization_code';
   //   const url = 'https://api.instagram.com/oauth/access_token';
 
-  //   const response = await fetch(url, {
-  //     method: 'POST',
-  //     mode: 'no-cors',
-  //     headers: {
-  //       'Access-Control-Allow-Origin' : '*',
-  //       'Content-Type': 'application/x-www-form-urlencoded',
-  //     },
-  //     body: `client_id=${clientID}&client_secret=${clientSecret}&grant_type=${grantType}&redirect_uri=${redirectURI}&code=${authorizationCode}`
-  //   });
+  //   try {
+  //     const response = await axios.post(url, {
+  //       client_id: clientID,
+  //       client_secret: clientSecret,
+  //       grant_type: grantType,
+  //       redirect_uri: redirectURI,
+  //       code: authorizationCode,
+  //     }, {
+  //       headers: {
+  //         'Access-Control-Allow-Origin': '*',
+  //         'Content-Type': 'application/x-www-form-urlencoded'
+  //       }
+  //     });
 
-  //   const { access_token } = await response.json();
+  //     const { access_token } = response.data;
 
-  //   console.log("the response: ",response)
-  //   const profileResponse = await fetch(`https://graph.instagram.com/me?fields=id,username&access_token=${access_token}`);
-  //   const profile = await profileResponse.json();
-  //   console.log("the profile: ",profile)
-  //   return { access_token, profile };
+  //     console.log("the response: ", response.data)
+  //     const profileResponse = await axios.get(`https://graph.instagram.com/me?fields=id,username&access_token=${access_token}`);
+  //     const profile = profileResponse.data;
+  //     console.log("the profile: ", profile)
+
+  //     return { access_token, profile };
+  //   } catch (error) {
+  //     console.error(error);
+  //     throw new Error('Failed to fetch Instagram profile');
+  //   }
   // };
-  const fetchInstagramProfile = async (authorizationCode: any) => {
-    const clientID = '2040498642821593';
-    const clientSecret = '07fd638959f54656f00f2f71d9dee9ce';
-    const redirectURI = 'https://react-social-login-starter-kit.vercel.app/dashboard';
-    const grantType = 'authorization_code';
-    const url = 'https://api.instagram.com/oauth/access_token';
-
-    try {
-      const response = await axios.post(url, {
-        client_id: clientID,
-        client_secret: clientSecret,
-        grant_type: grantType,
-        redirect_uri: redirectURI,
-        code: authorizationCode,
-      }, {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      });
-
-      const { access_token } = response.data;
-
-      console.log("the response: ", response.data)
-      const profileResponse = await axios.get(`https://graph.instagram.com/me?fields=id,username&access_token=${access_token}`);
-      const profile = profileResponse.data;
-      console.log("the profile: ", profile)
-
-      return { access_token, profile };
-    } catch (error) {
-      console.error(error);
-      throw new Error('Failed to fetch Instagram profile');
-    }
-  };
   const router = useRouter();
   const { code } = router.query;
   console.log("The code:", code)
