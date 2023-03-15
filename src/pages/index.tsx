df21b4e5fc1d9ce2dd8801212579ecb8727c8f4a
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-03-15 10:29:54 +0200

    styles checking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,12 +20,12 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={'bg-slate-50 min-h-screen p-20'}>
+      <main className={'bg-slate-800 text-white min-h-screen p-20 pb-0'}>
         <div className={styles.description}>
           <div>
             <p className='flex gap-6'>
               Get started by
-              <Link target='_blank' href={'https://github.com/Manzi-Cedrick?tab=repositories'} className={'font-bold text-black'}>Starter Packet /</Link>
+              <Link target='_blank' href={'https://github.com/Manzi-Cedrick?tab=repositories'} className={'font-bold text-white'}>Starter Packet /</Link>
             </p>
           </div>
           <div>
@@ -39,8 +39,7 @@ const Home = () => {
             </a>
           </div>
         </div>
-
-        <div className={' p-20 text-center justify-center flex flex-col'}>
+        <div className={'p-20 pb-0 text-center justify-center flex flex-col'}>
           <div className="text-9xl font-semibold 
             bg-gradient-to-r bg-clip-text  text-transparent 
             from-indigo-500 via-purple-500 to-indigo-500
@@ -50,11 +49,13 @@ const Home = () => {
             <span className='text-9xl from-green-600  font-bold'>Social.</span>
             <span className='text-9xl font-bold'>Logins</span>
           </div>
-          <p className='py-10 px-24'>Starter Social Packet is the platform for frontend developers, providing the speed and reliability innovators need to create at the moment of inspiration.</p>
+          <p className='py-10 px-24 text-white'>Starter Social Packet is the platform for frontend developers, providing the speed and reliability innovators need to create at the moment of inspiration.</p>
         </div>
-
-        <div className={'flex justify-center py-10'}>
-          <button type="button" className="text-white bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-10 py-4 text-center mr-2 mb-2" onClick={testApplication}>Test it Out Now</button>
+        <div className='text-center'>
+          <p className='text-9xl'>🚀</p>
+        </div>
+        <div className={'flex justify-center py-14'}>
+          <button type="button" onClick={testApplication} className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Test Now</button>
         </div>
       </main>
     </>
