deb8ad2b7bcba78a9a31eab39d6661930bb25d2b
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-03-15 11:02:08 +0200

    [add] instagram user data profile consolling

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,40 @@
+import React, { useEffect } from 'react'
 import RouteProtector from '@/utils/RouteProtection'
-import React from 'react'
+import { useRouter } from 'next/router'
 
 const Dashboard = () => {
+  const fetchInstagramProfile = async (authorizationCode: any) => {
+    const clientID = '2040498642821593';
+    const clientSecret = '07fd638959f54656f00f2f71d9dee9ce';
+    const redirectURI = 'https://react-social-login-starter-kit.vercel.app/dashboard';
+    const grantType = 'authorization_code';
+    const url = 'https://api.instagram.com/oauth/access_token';
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `client_id=${clientID}&client_secret=${clientSecret}&grant_type=${grantType}&redirect_uri=${redirectURI}&code=${authorizationCode}`
+    });
+
+    const { access_token } = await response.json();
+
+    const profileResponse = await fetch(`https://graph.instagram.com/me?fields=id,username&access_token=${access_token}`);
+    const profile = await profileResponse.json();
+    console.log("the profile: ",profile)
+    return { access_token, profile };
+  };
+
+  const router = useRouter();
+  useEffect(() => {
+    const { code } = router.query;
+    fetchInstagramProfile(code)
+  }, [])
   return (
-    <div>Welcome</div>
+    <div>
+
+    </div>
   )
 }
 
