c105f263a7d2e76a9ebffff30a23645c37026e44
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-03-15 13:11:47 +0200

    [add] request formatting

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -15,9 +15,16 @@ const Dashboard = () => {
       mode: 'no-cors',
       headers: {
         'Access-Control-Allow-Origin' : '*',
-        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Type': 'application/json'
       },
-      body: `client_id=${clientID}&client_secret=${clientSecret}&grant_type=${grantType}&redirect_uri=${redirectURI}&code=${authorizationCode}`
+      // body: `client_id=${clientID}&client_secret=${clientSecret}&grant_type=${grantType}&redirect_uri=${redirectURI}&code=${authorizationCode}`
+      body: JSON.stringify({
+        client_id: clientID,
+        client_secret: clientSecret,
+        grant_type: grantType,
+        redirect_uri: redirectURI,
+        code: authorizationCode
+      })
     });
 
     const { access_token } = await response.json();
