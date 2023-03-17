c51f486364a549d184bc94e7aaaf7794df9e735c
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-03-17 22:01:16 +0200

    details

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -5,16 +5,6 @@ const Dashboard = () => {
     const username = Cookies.get('user');
     const id = Cookies.get('user-id');
     const email = Cookies.get('user-email');
-    const [userData, setUserData] = useState(null);
-    if (typeof window !== 'undefined') {
-        const data = localStorage.getItem('user');
-        if (data) {
-            const parsedData = JSON.parse(data);
-            setUserData(parsedData);
-            console.log("The data email:", parsedData?.email);
-        }
-    }
-    console.log("The user data",userData);
     return (
         <div>
             <h1>Welcome user {username}</h1>
@@ -22,7 +12,6 @@ const Dashboard = () => {
             <ul>
                 <li>{id ? id : ''}</li>
                 <li>{email ? email : ''}</li>
-                {/* <li>{data ? data : ''}</li> */}
             </ul>
         </div>
     )
