3f1c929d1fbdc8c7b78b0bdb65244c63baf543d5
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-03-17 21:51:01 +0200

    [add] additional data

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -6,6 +6,10 @@ const Dashboard = () => {
     const id = Cookies.get('user-id');
     const email = Cookies.get('user-email');
     const data = localStorage.getItem('user');
+    const userData = data ? JSON.parse(data) : null;
+    if(userData){
+        console.log(userData?.email)
+    }
     return (
         <div>
             <h1>Welcome user {username}</h1>
