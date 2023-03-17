d15621dd858e0d40d4d47bb26648048ba80cb8dc
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-03-17 11:13:34 +0200

    changes

--- src/utils/creds.ts
+++ src/utils/creds.ts
@@ -4,12 +4,12 @@ interface externalAuth {
     callback_url:string;
 }
 export const Twitch : externalAuth = {
-    client_id: `${process.env.SOCIAL_LOGIN_TWITCH_CLIENT_ID}`,
-    client_secret: `${process.env.SOCIAL_LOGIN_TWITCH_CLIENT_SECRET}`,
-    callback_url: `${process.env.SOCIAL_LOGIN_TWITCH_CLIENT_REDIRECT_URI}`
+    client_id: `${process.env.NEXT_PUBLIC_SOCIAL_LOGIN_TWITCH_CLIENT_ID}`,
+    client_secret: `${process.env.NEXT_PUBLIC_SOCIAL_LOGIN_TWITCH_CLIENT_SECRET}`,
+    callback_url: `${process.env.NEXT_PUBLIC_SOCIAL_LOGIN_TWITCH_CLIENT_REDIRECT_URI}`
 }
 export const Instagram : externalAuth = {
-    client_id: `${process.env.SOCIAL_LOGIN_INSTAGRAM_CLIENT_ID}`,
-    client_secret: `${process.env.SOCIAL_LOGIN_INSTAGRAM_CLIENT_SECRET}`,
-    callback_url: `${process.env.SOCIAL_LOGIN_INSTAGRAM_CLIENT_REDIRECT_URI}`
+    client_id: `${process.env.NEXT_PUBLIC_SOCIAL_LOGIN_INSTAGRAM_CLIENT_ID}`,
+    client_secret: `${process.env.NEXT_PUBLIC_SOCIAL_LOGIN_INSTAGRAM_CLIENT_SECRET}`,
+    callback_url: `${process.env.NEXT_PUBLIC_SOCIAL_LOGIN_INSTAGRAM_CLIENT_REDIRECT_URI}`
 }
